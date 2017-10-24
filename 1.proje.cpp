37c64e737f8daa3913e1f5f782e3b5938db8875b
besteguntugborucu <33027115+besteguntugborucu@users.noreply.github.com>
2017-10-24 20:04:23 +0300

    16.10.2017
    random

--- 1.proje.cpp
+++ 1.proje.cpp
@@ -1,9 +1,14 @@
 #include<stdio.h>
 #include <conio.h>
+#include <stdlib.h>
+#include <time.h>
 main()
 {
-	for (int i=0;i<100;i++)
-	printf("%d \n",i);
-
+	int sayi=0;
+	srand(time(NULL));
+	sayi=5+rand()%100;
+	sayi=3+rand()%5;
+	
+	printf("%d\n",sayi);
 getch();
 }
